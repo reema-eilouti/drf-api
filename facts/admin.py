99db72d179c2cfd47d75ec90e64ba4c0932dce6b
reema-eilouti <reema.eilouti@gmail.com>
2021-06-22 14:48:19 +0300

    added model

--- facts/admin.py
+++ facts/admin.py
@@ -1,3 +1,7 @@
+from facts.models import Fact
 from django.contrib import admin
+from .models import Fact
 
 # Register your models here.
+
+admin.site.register(Fact)
--- facts/models.py
+++ facts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Fact(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
