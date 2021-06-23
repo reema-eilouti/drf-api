d5b866900ad4c953531c40efeaae74183c7ef9fb
reema-eilouti <reema.eilouti@gmail.com>
2021-06-23 16:15:52 +0300

    lab31 done

--- facts_project/settings.py
+++ facts_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cklf722!jk(*)82%!*2p$4=*lq4!&o$ia75xhqu^#d+sxgz68w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
